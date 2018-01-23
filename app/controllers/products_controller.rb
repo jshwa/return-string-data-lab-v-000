4908c253aeb32ac7daf32e615c0dba8334354caa
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-23 01:22:14 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,15 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    product = Product.create(product_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
 end
