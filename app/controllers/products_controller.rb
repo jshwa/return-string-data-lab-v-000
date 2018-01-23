2e3b751c4fc71b265c97cd7b212d7c431319ae2a
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-23 01:31:35 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
 
   def inventory
     @product = Product.find(params[:id])
-    if @product.inventory
+    if @product.inventory > 0
       render :plain "true"
     else
       render :plain "false"
